9b7e93ba2db1750c618b182835a2aabb36b43a49
David Steger <d.steger@ymail.com>
2019-10-02 18:16:59 -0700

    functioning input field

---
+++ app.js
@@ -0,0 +1,50 @@
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+});
+
+// Initial array of gifs
+var gifs = ["Leonardo Dicaprio", "Adam Sandler", "Gal Gadot", "Will Smith"];
+
+// Function for displaying gif buttons
+function renderButtons() {
+
+    // Deleting the movie buttons prior to adding new movie buttons
+    // (this is necessary otherwise we will have repeat buttons)
+    $("#buttons-view").empty();
+
+    // Looping through the array of gifs
+    for (var i = 0; i < gifs.length; i++) {
+
+        // Then dynamicaly generating buttons for each actor in the array.
+        var button = $("<button>");
+        // Adding a class
+        button.addClass("btn btn-outline-secondary button");
+        // Adding a data-attribute with a value of the data-name at index i
+        button.attr("data-name", gifs[i]);
+        // Providing the button's text with a value of the gif at index i
+        button.text(gifs[i]);
+        // Adding the button to the HTML
+        $("#buttons-view").append(button);
+    }
+}
+
+// This function handles events where one button is clicked
+$(".gif-add").on("click", function (event) {
+    // Preventing the buttons default behavior when clicked (which is submitting a form)
+    event.preventDefault();
+    // Captures value of input field
+    var gifName = $("#gif-input").val().trim();
+
+    // Pushing the input into gifs array
+    gifs.push(gifName);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+});
+
+renderButtons();
